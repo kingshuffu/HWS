a7e44f295d93a11e78c0f9054df2e618031efa87
kingshuffu <65357232+kingshuffu@users.noreply.github.com>
2023-11-01 21:36:52 -0400

    Project 6

---
+++ Animations/Animations/AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Animations
+//
+//  Created by Ayo Shafau on 9/26/23.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
