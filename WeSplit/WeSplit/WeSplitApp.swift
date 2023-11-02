903004e8aeb85e4fcdf462c15428f003ae36efb1
kingshuffu <65357232+kingshuffu@users.noreply.github.com>
2023-11-01 21:15:57 -0400

    WeSplit - Project 1

---
+++ WeSplit/WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Ayo Shafau on 9/1/23.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
