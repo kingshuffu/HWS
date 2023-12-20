2fc5d7369d00614e754d7b27f92cd2d21eafcda4
King <king_shafau@knights.ucf.edu>
2023-12-20 14:55:19 -0800

    Initial Milestone 5 commit

---
+++ Milestone5/Milestone5/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Milestone5
+//
+//  Created by Ayo Shafau on 12/20/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Milestone5/Milestone5/Milestone5App.swift
@@ -0,0 +1,17 @@
+//
+//  Milestone5App.swift
+//  Milestone5
+//
+//  Created by Ayo Shafau on 12/20/23.
+//
+
+import SwiftUI
+
+@main
+struct Milestone5App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
